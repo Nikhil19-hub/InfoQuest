9f0f532c320153c2529e31fe00b646680cdbdcdd
Nikhil <nikhiil.kamble08@gmail.com>
2023-07-05 12:09:08 -0500

    minor css fixes

--- SearchEngine/src/App.jsx
+++ SearchEngine/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       {/* Root element */}
       <div className={dark ? "dark" : ""}>
         {/* Main content */}
-        <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200  min-h-screen min-w-full overflow-hidden">
+        <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200  min-h-screen  overflow-auto">
           {/* Navbar */}
           <Navbar dark={dark} setDark={setDark} />
 
